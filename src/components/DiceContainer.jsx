220b6da6cd33e359c83537c67571711b64aa09bb
Paul Henstridge <paulhenstridge@gmail.com>
2024-01-18 12:06:36 +0000

    adding dice holds

--- src/components/DiceContainer.jsx
+++ src/components/DiceContainer.jsx
@@ -14,6 +14,7 @@ const DiceBox = styled.section`
 const DiceContainer = () => {
 
     const [diceVals, setDiceVals] = useState(['?','?','?','?','?'])
+    const [holdList, setHoldList] = useState([false, false, false, false, false])
 
     const fetchDiceValues = async () => {
         try {
@@ -23,12 +24,27 @@ const DiceContainer = () => {
         } catch (error) {
             console.error('Error fetching data:', error);
         }
-    };
+    };  
+
+     const holdClicked = async (index) => {
+        try {
+            const response = await axios.post('http://localhost:8080/hold', {index:index})
+            console.log("response data --> ", response.data)
+            setHoldList(response.data.holdList)
+            console.log("hold list --> ", holdList)
+
+        }
+        catch (error) {
+        console.error('Error fetching data:', error);
+    }
+    }
+
+
 
 
     return ( 
     <DiceBox>
-        {Array.from({ length: 5 }, (_, index) => <Die key={index} index={index} value={diceVals[index]} />)
+        {Array.from({ length: 5 }, (_, index) => <Die key={index} index={index} value={diceVals[index]} holdClicked={holdClicked} isHeld={holdList[index]}/>)
 }
 
     <button onClick={fetchDiceValues}>Roll Dice</button>
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,19 +1,36 @@
 import { styled } from "styled-components";
 
+const FlexContainer = styled.div`
+    display:flex;
+    flex-direction: column;
+    align-items: center;
+`
+
 const DieBox = styled.div`
 width: max-content;
     aspect-ratio:1;
     border: 2px solid black;
     margin:1rem;
     padding: 1.5rem;
-
+`
+const HoldButton = styled.button`
+    background-color: ${props => props.isHeld ? 'yellow' : 'green'};
+    width: max content;
+    height: max-content;
+    /* margin-right: 1rem; */
 `
 
 
-const Die = ({value}) => {
-    return ( <DieBox>
+const Die = ({index, value, holdClicked, isHeld}) => {
+ console.log("die ", index, "isHeld = ", isHeld)
+    return ( 
+    <FlexContainer> 
+    <DieBox>
     <span>{value}</span>
-    </DieBox> );
+    </DieBox> 
+    <HoldButton onClick={() => holdClicked(index)} isHeld={isHeld}>HOLD</HoldButton>
+    </FlexContainer>
+    );
 }
  
 export default Die;
